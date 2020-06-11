7539e903ed4dd9d4c00959a1ef2ddb4f4ca8b120
Justin Largo <justinlargo7@gmail.com>
2020-06-10 20:03:30 -0400

    Messaging V2: Rooms/ Names are validated for prexisting conditions and messages are saved.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const { hostname } = require('os');
+const { RSA_PKCS1_PADDING } = require('constants');
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const port = process.env.PORT || 6969;
+
+const rooms = {};
 
 http.listen(port, () => {
 	console.log(`Draftster API running on ${port}`);
@@ -19,19 +24,60 @@ io.on('connection', (socket) => {
 	});
 
 	// The user creates a room
-	socket.on('createRoom', (roomId) => {
-		socket.join(roomId);
+	socket.on('createRoom', (data, fn) => {
+		const roomId = data.roomId;
+		const hostName = data.name;
+		// Check if room has been created
+		if (rooms[roomId] === undefined) {
+			// Create and join the room
+			socket.join(roomId);
+			rooms[roomId] = {
+				players: [hostName],
+				messages: [],
+			};
+			return fn({ roomExists: false });
+		} else {
+			return fn({ roomExists: true });
+		}
 	});
 
 	// The user joins a room
-	socket.on('joinRoom', (roomId) => {
-		socket.join(roomId);
+	socket.on('joinRoom', (data, fn) => {
+		const roomId = data.roomId;
+		const playerName = data.name;
+
+		// Check if room has been created
+		if (rooms.hasOwnProperty(roomId)) {
+			// Check if player name is available
+
+			if (!rooms[roomId].players.includes(playerName)) {
+				// Create and join the room
+				socket.join(roomId);
+				rooms[roomId].players.push(playerName);
+				const messageHistory = rooms[roomId].messages;
+				// Both fields are good!
+				// Send message history as well
+				return fn({
+					roomExists: true,
+					nameInUse: false,
+					messages: messageHistory,
+				});
+			} else {
+				// The room exists but the name is in use
+				return fn({ roomExists: true, nameInUse: true });
+			}
+		} else {
+			// The room does not exist
+			return fn({ roomExists: false, nameInUse: false });
+		}
 	});
 
 	// Message from client
 	socket.on('message', (data) => {
 		const message = data.message;
 		const roomId = data.roomId;
+		// Save message to room messages history
+		rooms[roomId].messages.push(message);
 		// Send to all clients
 		socket.to(roomId).emit('message', message);
 	});
